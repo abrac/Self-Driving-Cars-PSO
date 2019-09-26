bf4eb2e64271c5f854d4c7ee9eb186d4e9ad2a97
Chris Abraham <55248999+abracwan@users.noreply.github.com>
2019-09-26 22:00:39 +0200

    Added feelers to vehicle prefab
    Car is now able to sense distance to any obstacle colliding with 3 feelers.

--- DriveLineLearning/Assets/Scripts/Feelers_RayGenerator.cs
+++ DriveLineLearning/Assets/Scripts/Feelers_RayGenerator.cs
@@ -4,26 +4,55 @@ using UnityEngine;
 
 public class Feelers_RayGenerator : MonoBehaviour
 {
-
+    RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 5.0f))
+        //0 degrees
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 5.0f))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Hit at " + hit.distance);
+            Debug.Log("0deg Hit at " + hit.distance);
         }
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.blue);
-            Debug.Log("Did not Hit");
+            Debug.Log("0deg Did not Hit");
+        }
+
+        //45 degrees
+        Vector3 directionVector45 = transform.localPosition + new Vector3((float)(1 / System.Math.Sqrt(2)), 0, (float)(1 / System.Math.Sqrt(2)));
+        directionVector45 = transform.TransformVector(directionVector45);
+        if (Physics.Raycast(transform.position, directionVector45, out hit, 5.0f))
+        {
+            Debug.DrawRay(transform.position, directionVector45 * hit.distance, Color.yellow);
+            Debug.Log("45deg Hit at " + hit.distance);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, directionVector45 * 5, Color.blue);
+            Debug.Log("45deg Did not Hit");
+        }
+
+        //-45 degrees
+        Vector3 directionVectorNeg45 = transform.localPosition + new Vector3((float)(-1 / System.Math.Sqrt(2)), 0, (float)(1 / System.Math.Sqrt(2)));
+        directionVectorNeg45 = transform.TransformVector(directionVectorNeg45);
+        if (Physics.Raycast(transform.position, directionVectorNeg45, out hit, 5.0f))
+        {
+            Debug.DrawRay(transform.position, directionVectorNeg45 * hit.distance, Color.yellow);
+            Debug.Log("-45deg Hit at " + hit.distance);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, directionVectorNeg45 * 5, Color.blue);
+            Debug.Log("-45deg Did not Hit");
         }
     }
+
 }
