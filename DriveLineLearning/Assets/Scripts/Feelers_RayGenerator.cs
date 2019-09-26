084539855e39e664e2e91d662b2b1d8aaf0d8f91
Chris Abraham <55248999+abracwan@users.noreply.github.com>
2019-09-26 20:10:37 +0200

    Added ray generator script
    Only one feeler implimented.

---
+++ DriveLineLearning/Assets/Scripts/Feelers_RayGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feelers_RayGenerator : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 5.0f))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Hit at " + hit.distance);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.blue);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
