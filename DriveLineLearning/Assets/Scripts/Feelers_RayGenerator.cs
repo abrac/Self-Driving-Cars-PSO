606a9abf1e5a536479b02f6f5aa9c8c2470ed57a
Chris Abraham <55248999+abracwan@users.noreply.github.com>
2019-09-27 14:02:04 +0200

    Refactored Feelers code

--- DriveLineLearning/Assets/Scripts/Feelers_RayGenerator.cs
+++ DriveLineLearning/Assets/Scripts/Feelers_RayGenerator.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Feelers_RayGenerator : MonoBehaviour
 {
-    public float Dist0;
-    public float Dist45;
-    public float DistNeg45;
+    public float dist0;
+    public float dist45;
+    public float distNeg45;
     private float feelerLength = 8;
 
     RaycastHit hit;
@@ -23,12 +23,12 @@ public class Feelers_RayGenerator : MonoBehaviour
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, feelerLength))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Dist0 = hit.distance;
+            dist0 = hit.distance;
         }
         else
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * feelerLength, Color.blue);
-            Dist0 = -1;
+            dist0 = -1;
         }
 
         //45 degrees
@@ -37,12 +37,12 @@ public class Feelers_RayGenerator : MonoBehaviour
         if (Physics.Raycast(transform.position, directionVector45, out hit, feelerLength))
         {
             Debug.DrawRay(transform.position, directionVector45 * hit.distance, Color.yellow);
-            Dist45 = hit.distance;
+            dist45 = hit.distance;
         }
         else
         {
             Debug.DrawRay(transform.position, directionVector45 * feelerLength, Color.blue);
-            Dist45 = -1;
+            dist45 = -1;
         }
 
         //-45 degrees
@@ -51,12 +51,12 @@ public class Feelers_RayGenerator : MonoBehaviour
         if (Physics.Raycast(transform.position, directionVectorNeg45, out hit, feelerLength))
         {
             Debug.DrawRay(transform.position, directionVectorNeg45 * hit.distance, Color.yellow);
-            DistNeg45 = hit.distance;
+            distNeg45 = hit.distance;
         }
         else
         {
             Debug.DrawRay(transform.position, directionVectorNeg45 * feelerLength, Color.blue);
-            DistNeg45 = -1;
+            distNeg45 = -1;
         }
     }
 
