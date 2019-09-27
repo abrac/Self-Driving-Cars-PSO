05b0e0d918c81827b55b782d2b0df5d521af8395
Chris Abraham <55248999+abracwan@users.noreply.github.com>
2019-09-27 13:59:24 +0200

    Cleaned up feelers code
    Changed feeler hit distance properties to fields, so that they appear in inspector window.
    Added feelerLength field to easily change it.

--- DriveLineLearning/Assets/Scripts/Feelers_RayGenerator.cs
+++ DriveLineLearning/Assets/Scripts/Feelers_RayGenerator.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Feelers_RayGenerator : MonoBehaviour
 {
-    public float Dist0 { get; private set; }
-    public float Dist45 { get; private set; }
-    public float DistNeg45 { get; private set; }
+    public float Dist0;
+    public float Dist45;
+    public float DistNeg45;
+    private float feelerLength = 8;
 
     RaycastHit hit;
     // Start is called before the first frame update
@@ -19,43 +20,42 @@ public class Feelers_RayGenerator : MonoBehaviour
     void Update()
     {
         //0 degrees
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 5.0f))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, feelerLength))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
             Dist0 = hit.distance;
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.blue);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * feelerLength, Color.blue);
             Dist0 = -1;
         }
 
         //45 degrees
         Vector3 directionVector45 = transform.localPosition + new Vector3((float)(1 / System.Math.Sqrt(2)), 0, (float)(1 / System.Math.Sqrt(2)));
         directionVector45 = transform.TransformVector(directionVector45);
-        if (Physics.Raycast(transform.position, directionVector45, out hit, 5.0f))
+        if (Physics.Raycast(transform.position, directionVector45, out hit, feelerLength))
         {
             Debug.DrawRay(transform.position, directionVector45 * hit.distance, Color.yellow);
-            Debug.Log("45deg Hit at " + hit.distance);
             Dist45 = hit.distance;
         }
         else
         {
-            Debug.DrawRay(transform.position, directionVector45 * 5, Color.blue);
+            Debug.DrawRay(transform.position, directionVector45 * feelerLength, Color.blue);
             Dist45 = -1;
         }
 
         //-45 degrees
         Vector3 directionVectorNeg45 = transform.localPosition + new Vector3((float)(-1 / System.Math.Sqrt(2)), 0, (float)(1 / System.Math.Sqrt(2)));
         directionVectorNeg45 = transform.TransformVector(directionVectorNeg45);
-        if (Physics.Raycast(transform.position, directionVectorNeg45, out hit, 5.0f))
+        if (Physics.Raycast(transform.position, directionVectorNeg45, out hit, feelerLength))
         {
             Debug.DrawRay(transform.position, directionVectorNeg45 * hit.distance, Color.yellow);
             DistNeg45 = hit.distance;
         }
         else
         {
-            Debug.DrawRay(transform.position, directionVectorNeg45 * 5, Color.blue);
+            Debug.DrawRay(transform.position, directionVectorNeg45 * feelerLength, Color.blue);
             DistNeg45 = -1;
         }
     }
