074f7d8132e620eafd38382a4714448c36aec001
B34STM4ST3R <geoffreyolls@gmail.com>
2019-10-17 21:58:58 +0200

    Reworked car texures (waste of time, yes); changed car colliders; added a start line to one of the control points (want to make a starting block object); Added ExtensionMethods script and Distance tracker script

--- DriveLineLearning/Assets/Scripts/DistanceTracker/DistanceTracker.cs
+++ DriveLineLearning/Assets/Scripts/DistanceTracker/DistanceTracker.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class DistanceTracker : MonoBehaviour
 {
+    public float distanceTravelled = 0;
+    Vector3 lastPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        distanceTravelled = 0;
+        lastPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distanceTravelled += Vector3.Distance(transform.position,lastPosition);
+        lastPosition = transform.position;
     }
 }
