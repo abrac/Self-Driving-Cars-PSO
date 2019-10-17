ce2b4e99b0470959d37164bd124d9dd7ced8625a
JMilnerHowarth <55314095+JMilnerHowarth@users.noreply.github.com>
2019-10-17 15:56:25 +0200

    SpeedDisplay

---
+++ DriveLineLearning/Assets/Scripts/UI/Speed.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speed : MonoBehaviour
+{
+    public Text SpeedText;
+    private float lastposition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastposition = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = (transform.position.magnitude - lastposition)/Time.deltaTime;
+        lastposition = transform.position.magnitude;
+
+        string SpeedToShow = Mathf.Abs(((int)speed)).ToString();
+
+        SpeedText.text = SpeedToShow + "km/h";
+    }
+}
