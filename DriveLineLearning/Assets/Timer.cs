d9df868e3f0366cb4dc1cb20afa6c289bd1aa8b0
JMilnerHowarth <55314095+JMilnerHowarth@users.noreply.github.com>
2019-10-17 14:41:14 +0200

    Timer

---
+++ DriveLineLearning/Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimerText;
+    private float startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        TimerText.text = minutes + ":" + seconds;
+    }
+}
+
