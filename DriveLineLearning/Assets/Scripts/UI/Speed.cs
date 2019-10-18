74ad362f3fec94fa6d368fd80a9aa0dba1b7fb21
JMilnerHowarth <55314095+JMilnerHowarth@users.noreply.github.com>
2019-10-18 12:18:55 +0200

    UpdateSpeed

--- DriveLineLearning/Assets/Scripts/UI/Speed.cs
+++ DriveLineLearning/Assets/Scripts/UI/Speed.cs
@@ -6,20 +6,20 @@ using UnityEngine.UI;
 public class Speed : MonoBehaviour
 {
     public Text SpeedText;
-    private Vector3 lastposition;
+    public float speed2;
+    public GameObject car;
+
     // Start is called before the first frame update
     void Start()
     {
-        lastposition = transform.position;
+        //lastposition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float speed = Vector3.Distance(lastposition, transform.position)/Time.deltaTime;
-        lastposition = transform.position;
-
-        string SpeedToShow = Mathf.Abs(((int)speed)).ToString();
+        speed2 = car.GetComponent<UnityStandardAssets.Vehicles.Car.CarController>().CurrentSpeed;
+        string SpeedToShow = Mathf.Abs(((int)speed2)).ToString();
 
         SpeedText.text = SpeedToShow + "km/h";
     }
